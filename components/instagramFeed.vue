4ae7172f876e4950fe265a41b9cf7d2b8617af5a
Laura Davila <lnd337@gmail.com>
2017-10-04 20:13:02 -0500

    add instagram.js feed to homepage

---
+++ components/instagramFeed.vue
@@ -0,0 +1,57 @@
+<script>
+import Instafeed from 'instafeed.js'
+
+export default {
+  data () {
+    return {
+      // instagramFeed: [1, 2, 3, 4],
+      // clientID: 'e743c360a248473f8a0b188c88816792',
+    }
+  }
+}
+var feed = new Instafeed({
+  get: 'user',
+  userId: '31064017',
+  sortBy: 'most-recent',
+  resolution: 'low_resolution',
+  limit: 8,
+  clientId: 'e743c360a248473f8a0b188c88816792',
+  accessToken: '31064017.e743c36.afe76f91cd00496bab5edbc4c7df3242',
+  template: '<div class="crop"><a class="instagramImage" href="{{link}}" target="_blank"><img src="{{image}}" /></a></div>'
+})
+feed.run()
+</script>
+
+<template>
+  <div class="instagramFeed">
+    <h3><a href="https://www.instagram.com/katiecowden/" target="_blank">@KatieCowden</a></h3>
+    <div id="instafeed">
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.instagramFeed {
+  margin:auto;
+  display: block;
+  h3 {
+    line-height: 2rem;
+    font-size: 1.5rem;
+  }
+  #instafeed {
+    display: flex;
+    flex-wrap: wrap;
+  }
+}
+.crop {
+  padding: 2px;
+  overflow: hidden;
+  width: 25%;
+  height: 200px;
+  box-sizing: border-box;
+  img {
+    width: 100%;
+    height: auto;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,18 @@
 <template>
   <section class="container">
        <handlogo />
+       <instagramFeed />
   </section>
 </template>
 
 <script>
 import handlogo from '~/components/handlogo.vue'
+import instagramFeed from '~/components/instagramFeed.vue'
 
 export default {
   layout: 'home',
   components: {
-    handlogo
+    handlogo, instagramFeed
   }
 }
 </script>
