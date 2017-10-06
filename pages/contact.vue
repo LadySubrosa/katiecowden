608241b0a7219bdba588afad08a5d7269dc854c8
Laura Davila <lnd337@gmail.com>
2017-10-05 21:41:01 -0500

    test netlify form

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,10 +5,20 @@
       <h1 class="title">
         Contact</h1>
       
-      <form>
-        <input type="text" />
-        <button type="submit" />
-      </form>
+<form name="contact" action="thank-you" netlify>
+  <p>
+    <label>Your Name: <input type="text" name="name"></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email"></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
     </div>
   </section>
 </template>
