6a8a385bb437698079ef4e81d88fa074014f9e72
Laura Davila <lnd337@gmail.com>
2017-11-02 17:47:41 -0500

    add about bio

--- pages/about.vue
+++ pages/about.vue
@@ -4,7 +4,16 @@
  
       <h1 class="title">
         About</h1>
+
+        <div class="bio">
       
+        <p>Katie Cowden was raised in Bryan, Texas by an art teacher and a mad scientist, given mixed media supplies and books for every birthday, and brought up to take traditional materials and techniques from around the world to bend into something entirely new. </p>
+
+        <p>After ten years of teaching preschool, she took one laser cutting class and was ready to make her artistic bent into an artistic business. The end result was the eclectic, electric, and ultimately inimitable Killer Queen Designs.</p>
+
+        <p>Katie now wields light-based laser blades, barely-there flashes of silver needles, and teeny-tiny paint brushes with equal proficiency to make wearable and museum display worthy artwork in her East Austin studio. And as her repertoire of techniques and materials expands, so too does the Killer Queen empire. </p>
+      </div>
+
     </div>
   </section>
 </template>
