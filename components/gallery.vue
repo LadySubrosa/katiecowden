d56f55db2ea2f0aefe36b8e309a4096e6747bdaf
Lady Subrosa <lnd337@gmail.com>
2019-06-13 08:41:57 -0500

    Image hotfixes and overall performance improvements (#1)
    * try to fix how links behave
    
    * Fix bug
    
    * Add back in a div or 2
    
    * Use template for loop
    
    * Try to fix gallery images with links
    
    * try something crazy
    
    * crazy pt 2
    
    * crazy p3
    
    * crazy p4
    
    * try a focus event
    
    * click
    
    * Back to regular click event
    
    * try to check clicks on image
    
    * syntax is hard
    
    * syntax is hard 2
    
    * syntax is hard 3: straight to video
    
    * Update gallery.vue
    
    * namespacing
    
    * Update gallery.vue
    
    * 🐰 Performance enhancements. Use CDN. Get rid of fluff.

--- components/gallery.vue
+++ components/gallery.vue
@@ -9,28 +9,53 @@ export default {
       id: this.galleryContent.id
     }
   },
+  methods: {
+    triggerClick: function (e) {
+      e.target.click()
+    }
+  },
   props: ['galleryContent']
 }
 </script>
 
 <template>
-<div :id="id" class="gallery">
-  <h2>{{ galleryName }}</h2>
-  <p>{{ galleryDescription }}</p>
-  <div class="gallery-images" >
-  <div class="thumbnail" v-for="image in images" :style="`background-image :url(/gallery/${path}/thumbnail/${image.file});`">
-    <div v-if="image.url" class="instagram-link">
-    <a tabindex="0" :href="image.url" >
-        <img :alt="galleryName" :src="`/gallery/${path}/thumbnail/${image.file}`" />
-    </a>
-  </div>
-  <div class="gallery-link" v-else>
-      <img tabindex="0" v-img="{ src: `/gallery/${path}/fullsize/${image.file}`, group: galleryName, title: galleryName }" :src="`/gallery/${path}/thumbnail/${image.file}`" />
-  </div>
-  </div>
-  </div>
 
-</div>
+  <div :id="id" class="gallery">
+    <h2>{{ galleryName }}</h2>
+    <p>{{ galleryDescription }}</p>
+    <div class="gallery-images">
+      <template v-for="image in images">
+      <template v-if="image.url">
+        <a
+
+          tabindex="0"
+          :href="image.url"
+          title=""
+          target="_blank"
+          class="thumbnail"
+          :style="{ backgroundImage: 'url(' + require(`~/static/gallery/${path}/thumbnail/${image.file}`) + ')' }"
+        >
+          <span class="sr-only"></span>
+        </a>
+      </template>
+      <template v-else>
+        <img
+          class="thumbnail"
+          tabindex="0"
+          :style="{ backgroundImage: 'url(' + require(`~/static/gallery/${path}/thumbnail/${image.file}`) + ')' }"
+          alt=""
+          v-img="{
+            src: `/gallery/${path}/fullsize/${image.file}`,
+            group: galleryName,
+            title: galleryName
+          }"
+          v-on:keyup.enter="triggerClick"
+          :src="`/gallery/${path}/thumbnail/${image.file}`"
+        />
+      </template>
+      </template>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
@@ -43,55 +68,54 @@ p {
 }
 
 .gallery-images {
-    // display: flex;
-    // flex-wrap: wrap;
-    justify-content: space-between;
-    margin: auto;
-    max-width: 960px;
-  .thumbnail {
+  justify-content: space-between;
+  margin: auto;
+  max-width: 960px;
+  .thumbnail,
+  a.thumbnail {
     box-sizing: border-box;
-    width: percentage(1/4)-.5;
+    width: percentage(1/4)-0.5;
     height: 0;
-    margin: .25%;
-    padding-top: percentage(1/4)-.5;
-    background-position:center center;
-    background-repeat:no-repeat;
-    background-size:cover;
+    margin: 0.25%;
+    padding-top: percentage(1/4)-0.5;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
     align-content: center;
     display: inline-block;
     position: relative;
     cursor: pointer;
-    transition: filter .25s ease-in;
+    transition: filter 0.25s ease-in;
     -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
     filter: grayscale(100%);
-      &:hover {
-              -webkit-filter: grayscale(0%); /* Safari 6.0 - 9.0 */
+    &:hover,
+    &:focus {
+      -webkit-filter: grayscale(0%); /* Safari 6.0 - 9.0 */
       filter: grayscale(0%);
-      }
+    }
     @media screen and (max-width: 768px) {
       //display: block;
       margin-bottom: 1em;
       // width: 100%;
       // padding-top: 100%;
-      width: percentage(1/2)-.5;
+      width: percentage(1/2)-0.5;
       height: 0;
-      margin: .25%;
-      padding-top: percentage(1/2)-.5;
+      margin: 0.25%;
+      padding-top: percentage(1/2)-0.5;
       height: 0;
       filter: none;
     }
-    img {
-      position: absolute;
-      top: 0;
-      left: 0;
-      bottom: 0;
-      right: 0;
-      width: 100%;
-      height: 100%;
-       margin: 0;
-       opacity: 0;
-    }
-
   }
+}
+
+.sr-only {
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  clip: rect(1px 1px 1px 1px); // IE 6 and 7
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: polygon(0px 0px, 0px 0px, 0px 0px);
+  -webkit-clip-path: polygon(0px 0px, 0px 0px, 0px 0px);
+  overflow: hidden !important;
 }
 </style>
