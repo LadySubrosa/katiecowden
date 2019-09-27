d7b0a9a8f7bdad38106d8f20034243c2ea9eb34d
Laura Davila <lnd337@gmail.com>
2019-09-26 22:30:36 -0500

    abstract locations so we only need to update in one place

--- pages/about.vue
+++ pages/about.vue
@@ -28,52 +28,8 @@
       </div>
 
       <div class="katieLocaitons">
-      <h2>Where to Find Katie</h2>
+        <Locations />
 
-      <div class="category">
-      <h3>Online</h3>
-        <p>Instagram: <a href="https://www.instagram.com/katiecowden/" target="_blank">@katiecowden</a><br/>
-        <p>Jewelry on <a href="https://www.etsy.com/shop/KillerQueenJewelry" target="_blank">Etsy.</a></p>
-      </div>
-      <div class="category">
-        <h3>Retail</h3>
-
-    
-      <h4>Austin, TX</h4>
-        <ul>
-          <li>Pink and Silver Fashion (all locations)</li>
-          <li>Revival Vintage</li>
-          <li>Art for the People Gallery</li>
-          <li>Secret Oktober</li>
-          <li>Triple Z Threadz/Limbo (all locations)</li>
-          <li>Plant Party</li>
-          <li>Tillery Plant Co</li>
-          <li>Recspec Gallery</li>
-          <li>The Mall</li>
-          <li>Mexic-Arte Museum</li>
-          <li>Museum of the Weird</li>
-          <li>The Austin Shaker</li>
-          <li>Stardust Vintage</li>
-          <li>The Glass Coffin</li>
-          <li>Fine Southern Gentlemen</li>
-          <li>Austin Book Arts Center</li>
-          <li>Black Orchid Salon</li>
-          <li>Wizard Hat Smoke Shop</li>
-        </ul>
-
-      <h4>Texas</h4>
-        <ul>
-          <li>Bird and Pear (San Antonio)</li>
-          <li>Dallas Pinup (Dallas)</li>
-          <li>Space Montrose (Houston)</li>
-          <li>Frame Gallery (Bryan)</li>
-          <li>Fine Southern Gentlemen (San Marcos)</li>
-          <li>Hello Tallulah (San Antonio)</li>
-          <li>Lashing Out (Dallas)</li>
-          <li>AR Workshop (Pearland)</li>
-        </ul>
-        
-</div>
       <div class="category">
           <h3>Custom Inquiries</h3>
           <p>Katie is also available for custom work any time.<br/><router-link to="contact">Contact to set up an appointment in her East Austin studio.</router-link> </p>
@@ -84,13 +40,13 @@
 </template>
 
 <script>
-import findKatie from '~/components/findKatie.vue'
+import locations from '~/components/locations.vue'
 export default {
   head: {
     title: 'About Katie Cowden'
   },
   components: {
-    findKatie
+    Locations: locations
   }
 }
 </script>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,151 +1,131 @@
+<script>
+
+import locations from '~/components/locations.vue'
+export default {
+  components: {
+    Locations: locations
+  }
+}
+</script>
+
 <template>
   <section class="container">
-      <div class="contact-content">
-    <div class="contact-form">
-      <h1 class="title">
-        Contact</h1>
+    <div class="contact-content">
+      <div class="contact-form">
+        <h1 class="title">
+          Contact</h1>
 
-      <p>Looking for something in particular? Contact Katie for information on custom laser cutting work, portrait photography, or other creative endeavors.</p>
+          <p>Looking for something in particular? Contact Katie for information on custom laser cutting work, portrait photography, or other creative endeavors.</p>
 
-<form name="contact" method="post" action="https://formspree.io/kt.cowden@gmail.com">
-  <input type="hidden" name="_cc" value="lnd337@gmail.com" />
-  <input type="hidden" name="_format" value="plain" />
-  <input type="text" name="_gotcha" style="display:none" />
-  <div class="input-group">
-    <label>Your Name</label>
-    <input type="text" name="name">
-  </div>
-  <div class="input-group">
-    <label>Your Email Address</label>
-    <input type="email" name="_replyto">
-  </div>
-  <div class="input-group">
-    <label>Subject</label>
+          <form name="contact" method="post" action="https://formspree.io/kt.cowden@gmail.com">
+            <input type="hidden" name="_cc" value="lnd337@gmail.com" />
+            <input type="hidden" name="_format" value="plain" />
+            <input type="text" name="_gotcha" style="display:none" />
+            <div class="input-group">
+              <label>Your Name</label>
+              <input type="text" name="name">
+            </div>
+            <div class="input-group">
+              <label>Your Email Address</label>
+              <input type="email" name="_replyto">
+            </div>
+            <div class="input-group">
+              <label>Subject</label>
 
-      <div class="radio_group"><input type="radio" name="_subject" value="Custom Laser Cut via Katiemakesart.com" id="subject1"><label for="subject1">Custom Laser Work</label></div>
-      <div class="radio_group">
-      <input type="radio" name="_subject" value="Photography via Katiemakesart.com" id="subject2"><label for="subject2">Portrait photography</label></div>
-      <div class="radio_group">
-      <input type="radio" name="_subject" value="Gallery via Katiemakesart.com"id="subject3"><label for="subject3">Gallery Display</label></div>
-      <div class="radio_group">
-      <input type="radio" name="_subject" value="General Contact via Katiemakesart.com"id="subject4"><label for="subject4">Other</label></div>
-    </select>
-  </div>
-  <div class="input-group">
-    <label>Message</label>
-    <textarea name="message"></textarea>
-  </div>
-  <div class="input-group">
-    <button type="submit">Contact Katie</button>
-  </div>
-</form>
- <img src="~/static/bio/paintingjewelry.jpg" alt="Photo of Katie Cowden's art supplies" />
-</div>
-           <div class="photo">
- <img src="~/static/bio/contact.jpg" alt="Photo of Katie Cowden's painting supplies" />
- <div class="findKatie">
+              <div class="radio_group"><input type="radio" name="_subject" value="Custom Laser Cut via Katiemakesart.com" id="subject1"><label for="subject1">Custom Laser Work</label></div>
+              <div class="radio_group">
+                <input type="radio" name="_subject" value="Photography via Katiemakesart.com" id="subject2"><label for="subject2">Portrait photography</label></div>
+                <div class="radio_group">
+                  <input type="radio" name="_subject" value="Gallery via Katiemakesart.com"id="subject3"><label for="subject3">Gallery Display</label></div>
+                  <div class="radio_group">
+                    <input type="radio" name="_subject" value="General Contact via Katiemakesart.com"id="subject4"><label for="subject4">Other</label></div>
+                  </select>
+                </div>
+                <div class="input-group">
+                  <label>Message</label>
+                  <textarea name="message"></textarea>
+                </div>
+                <div class="input-group">
+                  <button type="submit">Contact Katie</button>
+                </div>
+              </form>
+              <img src="~/static/bio/paintingjewelry.jpg" alt="Photo of Katie Cowden's art supplies" />
+            </div>
+            <div class="photo">
+              <img src="~/static/bio/contact.jpg" alt="Photo of Katie Cowden's painting supplies" />
 
-      <div class="katieLocaitons">
-      <h2>Where to Find Katie</h2>
-
-      <div class="category">
-      <h3>Online</h3>
-        <p>Instagram: <a href="https://www.instagram.com/katiecowden/" target="_blank">@katiecowden</a><br/>
-        <p>Jewelry on <a href="https://www.etsy.com/shop/KillerQueenJewelry" target="_blank">Etsy.</a></p>
-      </div>
-      <div class="category">
-        <h3>Retail</h3>
-
-      <h4>Austin, TX</h4>
-        <ul>
-          <li>Pink and Silver Fashion (all locations)</li>
-          <li>Revival Vintage</li>
-          <li>Art for the People Gallery</li>
-          <li>Secret Oktober</li>
-          <li>Triple Z Threadz/Limbo (all locations)</li>
-          <li>Plant Party</li>
-          <li>Tillery Plant Co</li>
-          <li>Recspec Gallery</li>
-          <li>The Mall</li>
-          <li>Mexic-Arte Museum</li>
-          <li>Museum of the Weird</li>
-          <li>The Austin Shaker</li>
-          <li>Stardust Vintage</li>
-          <li>The Glass Coffin</li>
-          <li>Fine Southern Gentlemen</li>
-          <li>Austin Book Arts Center</li>
-          <li>Black Orchid Salon</li>
-          <li>Wizard Hat Smoke Shop</li>
-        </ul>
-
-      <h4>Texas</h4>
-        <ul>
-          <li>Bird and Pear (San Antonio)</li>
-          <li>Dallas Pinup (Dallas)</li>
-          <li>Space Montrose (Houston)</li>
-          <li>Frame Gallery (Bryan)</li>
-          <li>Fine Southern Gentlemen (San Marcos)</li>
-          <li>Hello Tallulah (San Antonio)</li>
-          <li>Lashing Out (Dallas)</li>
-          <li>AR Workshop (Pearland)</li>
-        </ul>
-        
-        <p>Katie's line is available for wholesale or consignment. Please contact her for more information.</p>
-</div>
-      </div>
-    </div>
-</div>
-</div>
-</div>
+              <div class="findKatie">
+                <Locations />
+                <div class="category">
+                <p>Katie's line is available for wholesale or consignment. Please contact her for more information.</p>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
 
 
-  </section>
-</template>
+        </section>
+      </template>
 
-<style lang="scss">
+      <style lang="scss">
+      .category {
+              margin-bottom: 2em;
+              h4 {
+                font-size: .9em;
+                font-weight: 600;
+                text-transform: uppercase;
+                color: #757575;
+              }
+              ul {
+                margin-bottom: 1em;
+                display: block;
+                padding-bottom: 1em;
+                border-bottom: 2px solid #e8e8e8;
+              }
+          }
 
-
-  .contact-content {
-    display: flex;
-    @media screen and (max-width: 768px){
-      display: block;
-    }
-    .contact-form {
-      width: 45%;
-      margin-right: 5%;
-      @media screen and (max-width: 768px){
-        width: 100%;
-        margin: 0 auto;
+      .contact-content {
+        display: flex;
+        @media screen and (max-width: 768px){
+          display: block;
+        }
+        .contact-form {
+          width: 45%;
+          margin-right: 5%;
+          @media screen and (max-width: 768px){
+            width: 100%;
+            margin: 0 auto;
+          }
+          img {
+            width: 100%;
+            height: auto;
+          }
+          h1 {
+            text-align: left;
+            font-size: 3em;
+            margin-bottom: .5em;
+          }
+          p {
+            font-size: 1.25em;
+            line-height: 1.5em;
+            margin-bottom: 1em;
+          }
+        }
+        .photo {
+          box-sizing: border-box;
+          padding-left: 2em;
+          margin: 0;
+          width: 50%;
+          @media screen and (max-width:768px) {
+            width: 100%;
+            margin: auto;
+            padding: 1em 0;
+          }
+          img {
+            width: 100%;
+          }
+        }
       }
-      img {
-        width: 100%;
-        height: auto;
-      }
-      h1 {
-        text-align: left;
-        font-size: 3em;
-        margin-bottom: .5em;
-      }
-      p {
-        font-size: 1.25em;
-        line-height: 1.5em;
-        margin-bottom: 1em;
-      }
-    }
-    .photo {
-      box-sizing: border-box;
-      padding-left: 2em;
-      margin: 0;
-      width: 50%;
-      @media screen and (max-width:768px) {
-        width: 100%;
-        margin: auto;
-        padding: 1em 0;
-      }
-      img {
-        width: 100%;
-      }
-    }
-  }
 
-</style>
+      </style>
